2b33694954dcba0acd5808cfaa2b0a8b280cb4d2
Sinan Can Özer <sinancemozer@gmail.com>
2025-02-24 11:00:43 +0300

    feat:fonts and colors created

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -14,7 +14,9 @@ export default function App({ Component, pageProps }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={`${montserrat.variable} font-sans`}>
+      <main
+        className={`${montserrat.variable} font-mont bg-light w-full min-h-screen`}
+      >
         <Component {...pageProps} />
       </main>
     </>
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,13 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["var(--font-mont)"],
+        mont: ["var(--font-mont)"],
+      },
+      colors: {
+        dark: "#1b1b1b",
+        light: "#f5f5f5",
+        primary: "#B63E96", // 240,86,199
+        primaryDark: "#58E6D9", // 80,230,217
       },
     },
   },
