376c8c639e844320b5b068ff8899fcef3ddc8942
Sinan Can Özer <sinancemozer@gmail.com>
2025-02-26 15:33:49 +0300

    feat:social icons next

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,21 @@
 import Link from "next/link";
 import React from "react";
 import Logo from "./Logo";
+import { useRouter } from "next/router";
 
 const CustomLink = ({ href, title, className = "" }) => {
+  const router = useRouter();
   return (
-    <Link href={href} className={`${className}`}>
+    <Link href={href} className={`${className} relative group`}>
       {title}
+      <span
+        className={`
+        h-[1px] inline-block w-0 bg-dark absolute left-0 -bottom-0.5 
+        group-hover:w-full transition-[width] ease duration-300 
+        ${router.asPath === href ? "w-full" : "w-0"}`}
+      >
+        &nbsp;
+      </span>
     </Link>
   );
 };
@@ -15,9 +25,9 @@ const Navbar = () => {
     <header className="w-full px-32 py-8 font-medium flex items-center justify-between">
       <nav>
         <CustomLink href="/" title="Home" className="mr-4" />
-        <CustomLink href="/about" title="About" />
-        <CustomLink href="/projects" title="Projects" />
-        <CustomLink href="/articles" title="Articles" />
+        <CustomLink href="/about" title="About" className="mx-4" />
+        <CustomLink href="/projects" title="Projects" className="mx-4" />
+        <CustomLink href="/articles" title="Articles" className="ml-4" />
       </nav>
       <nav>
         <Link href="/" target={"_blank"}>
