06028915a0fb204365b8a9b67d130a516ab9bb00
Sinan Can Özer <sinancemozer@gmail.com>
2025-02-26 14:16:11 +0300

    feat:logo components adjusting 31.29 leftover

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,16 +2,23 @@ import Link from "next/link";
 import React from "react";
 import Logo from "./Logo";
 
+const CustomLink = ({ href, title, className = "" }) => {
+  return (
+    <Link href={href} className={`${className}`}>
+      {title}
+    </Link>
+  );
+};
+
 const Navbar = () => {
   return (
     <header className="w-full px-32 py-8 font-medium flex items-center justify-between">
       <nav>
-        <Link href="/">Home</Link>
-        <Link href="/about">About</Link>
-        <Link href="/projects">Projects</Link>
-        <Link href="/articles">Articles</Link>
+        <CustomLink href="/" title="Home" className="mr-4" />
+        <CustomLink href="/about" title="About" />
+        <CustomLink href="/projects" title="Projects" />
+        <CustomLink href="/articles" title="Articles" />
       </nav>
-      <Logo/>
       <nav>
         <Link href="/" target={"_blank"}>
           T
@@ -32,6 +39,9 @@ const Navbar = () => {
           T
         </Link>
       </nav>
+      <div className="absolute left-[50%] top-2 translate-x-[-50%]">
+        <Logo />
+      </div>
     </header>
   );
 };
