8867980dcdedf26b3ed14a51e53e7713b546fd2e
Sinan Can Özer <sinancemozer@gmail.com>
2025-03-06 14:17:40 +0300

    feat: next config adjustments

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = {
+  compiler: {
+    styledComponents: true,
+  },
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false };
+    return config;
+  },
+};
--- src/components/AnimatedText.js
+++ src/components/AnimatedText.js
@@ -1,17 +1,26 @@
 import React from "react";
+import { motion } from "framer-motion";
+
+const quote = {
+  initial: { opacity: 0 },
+  animate: { opacity: 1, transition: { delay: 0.5 } },
+};
 
 const AnimatedText = ({ text, className = "" }) => {
   return (
     <div className="w-full mx-auto py-2 flex items-center justify-center text-center overflow-hidden">
-      <h1
+      <motion.h1
         className={`inline-block w-full text-dark font-bold capitalize text-8xl ${className}`}
+        variants={quote}
+        initial="initial"
+        animate="animate"
       >
-        {text.split("  ").map((word, index) => (
+        {text.split(" ").map((word, index) => (
           <span key={word + "-" + index} className="inline-block">
             {word}&nbsp;
           </span>
         ))}
-      </h1>
+      </motion.h1>
     </div>
   );
 };
