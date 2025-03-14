28a206bc974291b00e896929de958fcba5288928
Sinan Can Özer <sinancemozer@gmail.com>
2025-03-14 13:02:26 +0300

    feat: tailwind configuration

---
+++ src/components/Skills.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Skills = () => {
+  return (
+    <>
+      <h2 className="font-bold text-8xl mt-64 w-full text-center">Skills</h2>
+      <div className="w-full h-screen relative flex items-center justify-center rounded-full bg-circularLight">
+        <div className="flex items-center justify-center rounded-full font-semibold bg-dark text-light">
+          Web
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Skills;
