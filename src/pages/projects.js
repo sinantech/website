915de840e52df318a9fa1d1550dec95356239eb3
Sinan Can Özer <sinancemozer@gmail.com>
2025-03-19 17:58:31 +0300

    projects page v0.1

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,7 +1,19 @@
+import Layout from "@/components/Layout";
+import Head from "next/head";
 import React from "react";
 
 const projects = () => {
-  return <div>projects</div>;
+  return (
+    <>
+      <Head>
+        <title>CodeBucks | Projects Page</title>
+        <meta name="description" content="any description" />
+        <main>
+          <Layout></Layout>
+        </main>
+      </Head>
+    </>
+  );
 };
 
 export default projects;
