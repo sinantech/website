524a80c24f90e56eaf9f3a0f33e5117db5c3562a
Sinan Can Özer <sinancemozer@gmail.com>
2025-03-28 13:50:59 +0300

    v0.9 articles page 3.03.31 leftover

--- src/pages/articles.js
+++ src/pages/articles.js
@@ -10,9 +10,13 @@ const articles = () => {
         <title>CodeBucks | Articles Page</title>
         <meta name="description" content="any description" />
       </Head>
-      <main>
-        <Layout>
+      <main className="w-full mb-16 flex flex-col items-center justify-center overflow-hidden">
+        <Layout className="pt-16">
           <AnimatedText text="Words Can Change The World!" className="mb-16" />
+          <ul className="grid grid-cols-2 gap-16">
+            <li>Featured articles-1</li>
+            <li>Featured articles-2</li>
+          </ul>
         </Layout>
       </main>
     </>
